af7809f3917e37356776f377c46d19ea9d83f7ab
Kelvin <ngotho.kelvin@yahoo.com>
2023-05-25 12:55:37 +0300

    Add test case for reverse method

--- tests/solver_spec.rb
+++ tests/solver_spec.rb
@@ -15,5 +15,11 @@ describe Solver do
         it 'Returns 1 when argument passed is 0' do
           expect(solver.factorial(0)).to eq(1)
         end
-      end
+    end
+
+    describe '#reverse' do
+        it 'returns the reverse of a word' do
+          expect(solver.reverse('hello')).to eq('olleh')
+        end
+    end
 end      
