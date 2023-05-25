558b325f522683aaba371d841b8bae7bd3a29483
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-25 13:35:13 +0300

    Implement FizzBuzz method

--- solver.rb
+++ solver.rb
@@ -8,4 +8,12 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'buzz' if (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+
+    num.to_s
+  end
 end
--- tests/solver_spec.rb
+++ tests/solver_spec.rb
@@ -1,28 +1,28 @@
 require_relative '../solver'
 
 describe Solver do
-    let(:solver) { Solver.new }
+  let(:solver) { Solver.new }
 
-    describe '#factorial' do
-        it 'returns the factorial of a positive number' do
-          expect(solver.factorial(5)).to eq(120)
-        end
-    
-        it 'raises an error when given a negative number' do
-          expect { solver.factorial(-5) }.to raise_error(StandardError, 'NegativeNumberFactorialError')
-        end
-    
-        it 'Returns 1 when argument passed is 0' do
-          expect(solver.factorial(0)).to eq(1)
-        end
+  describe '#factorial' do
+    it 'returns the factorial of a positive number' do
+      expect(solver.factorial(5)).to eq(120)
     end
 
-    describe '#reverse' do
-        it 'returns the reverse of a word' do
-          expect(solver.reverse('hello')).to eq('olleh')
-        end
+    it 'raises an error when given a negative number' do
+      expect { solver.factorial(-5) }.to raise_error(StandardError, 'NegativeNumberFactorialError')
     end
-    describe 'fizzbuzz' do
+
+    it 'Returns 1 when argument passed is 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+  end
+
+  describe '#reverse' do
+    it 'returns the reverse of a word' do
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+  end
+  describe 'fizzbuzz' do
     it 'returns fizz when argument passed is divisible by 3' do
       expect(solver.fizzbuzz(3)).to eq('fizz')
     end
@@ -39,4 +39,4 @@ describe Solver do
       expect(solver.fizzbuzz(7)).not_to eq(7)
     end
   end
-end  
+end
